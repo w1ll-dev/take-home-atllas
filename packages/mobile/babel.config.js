fdf77fb3d941aaf9c35dfc062f5272dce32e79b6
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-15 18:56:02 -0300

    chore(project-setup): add app relative paths

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -2,5 +2,33 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            "@assets": "./src/assets",
+            "@hooks": "./src/hooks",
+            "@screens": "./src/screens",
+            "@styles": "./src/styles",
+            "@providers": "./src/components/providers",
+            "@atoms": "./src/components/atoms",
+            "@molecules": "./src/components/molecules",
+            "@organisms": "./src/components/organisms",
+            "@navigation": "./src/navigation",
+          },
+        },
+      ],
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
