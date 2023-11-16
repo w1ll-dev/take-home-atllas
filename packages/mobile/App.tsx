7b948d060c7f293d939f457ce568a583bc013012
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-16 18:59:59 -0300

    feat(landing-screen): remove dead code from App.tsx

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,33 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./src/screens/home/Home";
-import WebView from "./src/screens/webview/WebView";
-import Login from "./src/screens/auth/Login";
-import Register from "./src/screens/auth/Register";
-
-export type StackScreens = {
-  Home: undefined;
-  Login: undefined;
-  Register: undefined;
-  App: undefined;
-};
-
-export const Stack = createNativeStackNavigator<StackScreens>();
+import { Navigation } from "@navigation";
+import { AppProvider } from "@providers";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen name="App" component={WebView} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
+    <AppProvider>
+      <Navigation />
+    </AppProvider>
   );
 }
