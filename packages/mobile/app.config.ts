1847208805749324d6e47157acafcc69d30c5005
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-15 21:31:37 -0300

    chore(project-setup): add tamagui component lib
    Using tamagui component lib (https://tamagui.dev/) we can implement default code style easily for
    all application

---
+++ packages/mobile/app.config.ts
@@ -0,0 +1,8 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  slug: "my-app",
+  name: "My App",
+  userInterfaceStyle: "automatic",
+});
