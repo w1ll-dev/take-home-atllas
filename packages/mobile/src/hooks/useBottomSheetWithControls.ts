7e6275466a89c9796cba9667d12c117847db5166
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-17 09:12:14 -0300

    feat(landing-screen): add hook to handle bottom sheet behavior

---
+++ packages/mobile/src/hooks/useBottomSheetWithControls.ts
@@ -0,0 +1,30 @@
+import BottomSheet from "@gorhom/bottom-sheet";
+import { useCallback, useRef } from "react";
+import { Keyboard } from "react-native";
+
+const useBottomSheetWithControls = () => {
+  const sheetRef = useRef<BottomSheet>(null);
+
+  const handleOpenBottomSheet = useCallback(
+    () => sheetRef.current?.expand(),
+    [],
+  );
+
+  const handleCloseBottomSheet = useCallback(
+    () => sheetRef.current?.close(),
+    [],
+  );
+
+  const handleOnClose = useCallback(() => {
+    Keyboard.dismiss();
+  }, []);
+
+  return {
+    sheetRef,
+    handleOpenBottomSheet,
+    handleCloseBottomSheet,
+    handleOnClose,
+  };
+};
+
+export { useBottomSheetWithControls };
