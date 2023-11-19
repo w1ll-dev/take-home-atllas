5880937fc9cde7306801b9e5eb3ce396f3626251
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-19 14:05:11 -0300

    feat(landing-screen): add props to image component

--- packages/mobile/src/components/atoms/Image/Image.tsx
+++ packages/mobile/src/components/atoms/Image/Image.tsx
@@ -1,10 +1,10 @@
 import { MediaDimensions } from "@assets/images/imageDimensions";
 import { ImageName, Images } from "@assets/images/imageName";
 import {
-  ImageProps as RNImageProps,
-  TouchableOpacityProps,
   Image as RNImage,
+  ImageProps as RNImageProps,
   TouchableOpacity,
+  TouchableOpacityProps,
 } from "react-native";
 
 type ImageProps = Omit<RNImageProps, "source"> &
@@ -13,16 +13,15 @@ type ImageProps = Omit<RNImageProps, "source"> &
     touchProps?: TouchableOpacityProps;
   };
 
-const Image = ({ image, touchProps }: ImageProps) => {
+const Image = ({ image, touchProps, ...props }: ImageProps) => {
   if (touchProps) {
     return (
       <TouchableOpacity {...touchProps}>
-        <RNImage source={Images[image]} />
+        <RNImage source={Images[image]} {...props} />
       </TouchableOpacity>
     );
   }
-
-  return <RNImage source={Images[image]} />;
+  return <RNImage source={Images[image]} {...props} />;
 };
 
 export { Image };
