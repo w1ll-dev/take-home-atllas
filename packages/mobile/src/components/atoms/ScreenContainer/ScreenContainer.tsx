f19ae8617fa3850ebc3a177bbf30983b98f0c204
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-19 14:07:38 -0300

    feat(landing-screen): improve screen container adding props

--- packages/mobile/src/components/atoms/ScreenContainer/ScreenContainer.tsx
+++ packages/mobile/src/components/atoms/ScreenContainer/ScreenContainer.tsx
@@ -1,20 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { ReactNode } from "react";
 import { useColorScheme, useWindowDimensions } from "react-native";
-import { YStack } from "tamagui";
+import { ColorTokens, YStack, YStackProps } from "tamagui";
 import { ScrollView } from "./styles";
 
 type ScreenContainerProps = {
   children: ReactNode;
-};
+  background: ColorTokens;
+} & YStackProps;
 
-const ScreenContainer = ({ children }: ScreenContainerProps) => {
+const ScreenContainer = ({
+  children,
+  background,
+  ...props
+}: ScreenContainerProps) => {
   const { width } = useWindowDimensions();
   const colorScheme = useColorScheme();
 
   return (
-    <ScrollView backgroundColor={"$background"}>
-      <YStack width={width} flex={1} backgroundColor={"$background"}>
+    <ScrollView backgroundColor={background}>
+      <YStack {...props} width={width} flex={1} backgroundColor={background}>
         <StatusBar style={colorScheme} />
         {children}
       </YStack>
--- packages/mobile/src/components/atoms/ScreenContainer/styles.ts
+++ packages/mobile/src/components/atoms/ScreenContainer/styles.ts
@@ -4,6 +4,6 @@ export const ScrollView = styled(TamaguiScrollView, {
   name: "TamaguiScrollView", // useful for debugging, and Component themes
   contentContainerStyle: {
     flexGrow: 1,
-    backgroundColor: "$background",
+    backgroundColor: "$background1",
   },
 });
