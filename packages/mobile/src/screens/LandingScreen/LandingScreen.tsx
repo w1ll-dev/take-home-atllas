b83d3daffad8e970c6782d290ad3ab3fbb532428
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-19 14:28:00 -0300

    feat(landing-screen): add landing-screen

--- packages/mobile/src/screens/LandingScreen/LandingScreen.tsx
+++ packages/mobile/src/screens/LandingScreen/LandingScreen.tsx
@@ -5,16 +5,15 @@ import BottomSheet, {
   BottomSheetBackdropProps,
 } from "@gorhom/bottom-sheet";
 import { useBottomSheetWithControls } from "@hooks/useBottomSheetWithControls";
-import { LoginForm } from "@organisms/LoginForm";
+import { useNavigationForRootStack } from "@navigation/hooks";
+import { LoginForm } from "@organisms/Forms";
 import { useCallback } from "react";
-import { KeyboardAvoidingView } from "react-native";
 import { Button, H1, Paragraph, YStack } from "tamagui";
 import { texts } from "./texts";
 
-const bottomSheetSnapPoints = ["50%"];
-
 const LandingScreen = () => {
   const { sheetRef, handleOnClose } = useBottomSheetWithControls();
+  const { navigate } = useNavigationForRootStack();
 
   const renderBackdrop = useCallback(
     (props: BottomSheetBackdropProps) => (
@@ -27,36 +26,45 @@ const LandingScreen = () => {
     [],
   );
 
+  const handleLoginPress = () => {
+    sheetRef.current.expand();
+  };
+
+  const handleRegisterPress = () => {
+    navigate("Register");
+  };
+
   return (
     <>
-      <ScreenContainer>
-        <YStack marginTop={"$10"} marginBottom={"$5"} alignItems="center">
+      <ScreenContainer background="$background1">
+        <YStack marginTop="$10" marginBottom="$5" alignItems="center">
           <Image image="darkLogo" />
         </YStack>
-        <YStack paddingHorizontal={"$5"} space={"$10"} paddingTop={"$10"}>
-          <H1 color={"$text"} fontFamily={"$heading"} fontSize={"$1"}>
+        <YStack paddingHorizontal="$5" space="$10" paddingTop="$10">
+          <H1 color="$text1" fontFamily="$heading" fontSize="$1">
             {texts.atllasOffersModern}
           </H1>
-          <Paragraph color={"$text"} textAlign="right" fontSize={"$3"}>
+          <Paragraph color="$text1" textAlign="right" fontSize="$3">
             {texts.meetTheNewStandard}
           </Paragraph>
         </YStack>
         <YStack flex={1} />
-        <YStack paddingHorizontal={"$5"} paddingVertical={"$10"} space={"$4"}>
+        <YStack paddingHorizontal="$5" paddingVertical="$10" space="$4">
           <Button
-            height={"$6"}
-            color={"$text"}
-            size={"$2"}
-            borderColor={"$royalBlue"}
-            borderWidth={"$1"}
-            onPress={() => sheetRef.current.expand()}>
+            height="$6"
+            color="$text1"
+            size="$2"
+            borderColor="$royalBlue"
+            borderWidth="$1"
+            onPress={handleLoginPress}>
             {texts.login}
           </Button>
           <Button
-            height={"$6"}
-            color={"$text"}
-            size={"$2"}
-            backgroundColor={"$royalBlue"}>
+            height="$6"
+            color="$text1"
+            size="$2"
+            backgroundColor="$royalBlue"
+            onPress={handleRegisterPress}>
             {texts.register}
           </Button>
         </YStack>
@@ -67,10 +75,8 @@ const LandingScreen = () => {
         index={-1}
         enablePanDownToClose
         backdropComponent={renderBackdrop}
-        snapPoints={bottomSheetSnapPoints}>
-        <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
-          <LoginForm />
-        </KeyboardAvoidingView>
+        snapPoints={["60%"]}>
+        <LoginForm />
       </BottomSheet>
     </>
   );
---
+++ packages/mobile/src/screens/LandingScreen/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const bottomSheetScrollViewStyles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+});
+
+export { bottomSheetScrollViewStyles };
