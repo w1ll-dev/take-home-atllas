8776d94bd4076c8315b68a8357941a8cd64147e5
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-20 15:21:48 -0300

    feat(register-query): add register route
    On this commit we implement the register route, to improve the code style we create get session
    method and implement this for login and register routes.

--- packages/back-end/src/routes/auth.ts
+++ packages/back-end/src/routes/auth.ts
@@ -1,26 +1,65 @@
-import IRoute from '../types/IRoute';
-import {Router} from 'express';
-import {compareSync} from 'bcrypt';
-import {attachSession} from '../middleware/auth';
-import {sequelize, Session, User} from '../services/db';
-import {randomBytes} from 'crypto';
+import IRoute from "../types/IRoute";
+import { Router } from "express";
+import { compareSync, hashSync } from "bcrypt";
+import { attachSession } from "../middleware/auth";
+import { sequelize, Session, User } from "../services/db";
+import { randomBytes } from "crypto";
+
+type SessionConfig = {
+  session?: Session;
+  sessionToken?: string;
+  error?: Error;
+};
+
+const getSessionConfig = async (
+  user,
+  response
+): Promise<SessionConfig | null> => {
+  // We now know the user is valid so it's time to mint a new session token.
+  const sessionToken = randomBytes(32).toString("hex");
+  let session;
+
+  try {
+    // Persist the token to the database.
+    session = await Session.create({
+      token: sessionToken,
+      user: user.dataValues.id,
+    });
+
+    if (!session) {
+      // Something broke on the database side. Not much we can do.
+      return {
+        error: passError("Returned session was nullish.", null, response),
+      };
+    }
+
+    return {
+      session,
+      sessionToken,
+    };
+  } catch (e) {
+    return {
+      error: passError("Failed to create session.", e, response),
+    };
+  }
+};
 
 const AuthRouter: IRoute = {
-  route: '/auth',
+  route: "/auth",
   router() {
     const router = Router();
     router.use(attachSession);
 
     // If we're authenticated, return basic user data.
-    router.get('/', (req, res) => {
+    router.get("/", (req, res) => {
       if (req.session?.token?.id) {
         const {
-          token: {token, ...session},
-          user: {password, ...user},
+          token: { token, ...session },
+          user: { password, ...user },
         } = req.session;
         return res.json({
           success: true,
-          message: 'Authenticated',
+          message: "Authenticated",
           data: {
             session,
             user,
@@ -29,37 +68,34 @@ const AuthRouter: IRoute = {
       } else {
         return res.json({
           success: false,
-          message: 'Not Authenticated',
+          message: "Not Authenticated",
         });
       }
     });
 
     // Attempt to log in
-    router.post('/login', async (req, res) => {
-      const {
-        username,
-        password,
-      } = JSON.parse(req.body);
-      
+    router.post("/login", async (req, res) => {
+      const { username, password } = req.body;
+
       if (!username || !password) {
         return res.status(400).json({
           success: false,
-          message: 'Missing username/password.',
+          message: "Missing username/password.",
         });
       }
 
       const user = await User.findOne({
         where: sequelize.where(
-          sequelize.fn('lower', sequelize.col('username')),
-          sequelize.fn('lower', username),
+          sequelize.fn("lower", sequelize.col("username")),
+          sequelize.fn("lower", username)
         ),
-      }).catch(err => console.error('User lookup failed.', err));
+      }).catch((err) => console.error("User lookup failed.", err));
 
       // Ensure the user exists. If not, return an error.
       if (!user) {
         return res.status(401).json({
           success: false,
-          message: 'Invalid username/password.',
+          message: "Invalid username/password.",
         });
       }
 
@@ -67,32 +103,20 @@ const AuthRouter: IRoute = {
       if (!compareSync(password, user.dataValues.password)) {
         return res.status(401).json({
           success: false,
-          message: 'Invalid username/password.',
+          message: "Invalid username/password.",
         });
       }
 
-      // We now know the user is valid so it's time to mint a new session token.
-      const sessionToken = randomBytes(32).toString('hex');
-      let session;
-      try {
-        // Persist the token to the database.
-        session = await Session.create({
-          token: sessionToken,
-          user: user.dataValues.id,
-        });
-      } catch (e) {
-        return passError('Failed to create session.', e, res);
-      }
+      const sessionConfig = await getSessionConfig(user, res);
 
-      if (!session) {
-        // Something broke on the database side. Not much we can do.
-        return passError('Returned session was nullish.', null, res);
+      if (!!sessionConfig.error) {
+        return sessionConfig.error;
       }
 
       // We set the cookie on the response so that browser sessions will
       // be able to use it.
-      res.cookie('SESSION_TOKEN', sessionToken, {
-        expires: new Date(Date.now() + (3600 * 24 * 7 * 1000)), // +7 days
+      res.cookie("SESSION_TOKEN", sessionConfig.sessionToken, {
+        expires: new Date(Date.now() + 3600 * 24 * 7 * 1000), // +7 days
         secure: false,
         httpOnly: true,
       });
@@ -104,20 +128,50 @@ const AuthRouter: IRoute = {
       // in non-oauth flows :)
       return res.json({
         success: true,
-        message: 'Authenticated Successfully.',
+        message: "Authenticated Successfully.",
         data: {
-          token: sessionToken,
+          token: sessionConfig.sessionToken,
         },
       });
     });
 
     // Attempt to register
-    router.post('/register', (req, res) => {
-      // TODO
+    router.post("/register", async (req, res) => {
+      const { username, password, displayName } = req.body;
+
+      if (!username || !password || !displayName) {
+        return res.status(400).json({
+          success: false,
+          message: "Missing username/password/displayName.",
+        });
+      }
+
+      try {
+        // Create user
+        const user = await User.create({
+          displayName,
+          username,
+          password: hashSync(password, 12),
+          id: Number(Date.now()),
+          registered: new Date(),
+        });
+
+        const sessionConfig = await getSessionConfig(user, res);
+
+        return res.json({
+          success: true,
+          message: "Registered Successfully.",
+          data: {
+            token: sessionConfig.sessionToken,
+          },
+        });
+      } catch (e) {
+        return passError("Failed to create user.", e, res);
+      }
     });
 
     // Log out
-    router.post('/logout', (req, res) => {
+    router.post("/logout", (req, res) => {
       // TODO
     });
 
