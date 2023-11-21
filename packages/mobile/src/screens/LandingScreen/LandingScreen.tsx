086c9c38fe4c4e3b717895cf57cc9efba74628f7
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-20 22:14:38 -0300

    chore(add-unit-test): add new animation to landing screen

--- packages/mobile/src/screens/LandingScreen/LandingScreen.tsx
+++ packages/mobile/src/screens/LandingScreen/LandingScreen.tsx
@@ -10,10 +10,18 @@ import { LoginForm } from "@organisms/Forms";
 import { useCallback } from "react";
 import { Button, H1, Paragraph, YStack } from "tamagui";
 import { texts } from "./texts";
+import { useFadeInOut } from "@hooks/animations";
+import { MotiView } from "moti";
 
 const LandingScreen = () => {
-  const { sheetRef, handleOnClose } = useBottomSheetWithControls();
+  const { sheetRef, handleOnClose, handleOpenBottomSheet } =
+    useBottomSheetWithControls();
   const { navigate } = useNavigationForRootStack();
+
+  const { animate, from, transition } = useFadeInOut({
+    endOpacity: 1,
+    startY: -10,
+  });
 
   const renderBackdrop = useCallback(
     (props: BottomSheetBackdropProps) => (
@@ -26,10 +34,6 @@ const LandingScreen = () => {
     [],
   );
 
-  const handleLoginPress = () => {
-    sheetRef.current.expand();
-  };
-
   const handleRegisterPress = () => {
     navigate("Register");
   };
@@ -37,37 +41,41 @@ const LandingScreen = () => {
   return (
     <>
       <ScreenContainer background="$background1" statusBarStyle="light">
-        <YStack marginTop="$10" marginBottom="$5" alignItems="center">
-          <Image image="darkLogo" />
-        </YStack>
-        <YStack paddingHorizontal="$5" space="$10" paddingTop="$10">
-          <H1 color="$text1" fontFamily="$heading" fontSize="$1">
-            {texts.atllasOffersModern}
-          </H1>
-          <Paragraph color="$text1" textAlign="right" fontSize="$3">
-            {texts.meetTheNewStandard}
-          </Paragraph>
-        </YStack>
+        <MotiView from={from} animate={animate} transition={transition}>
+          <YStack marginTop="$10" marginBottom="$5" alignItems="center">
+            <Image image="darkLogo" />
+          </YStack>
+          <YStack paddingHorizontal="$5" space="$10" paddingTop="$10">
+            <H1 color="$text1" fontFamily="$heading" fontSize="$1">
+              {texts.atllasOffersModern}
+            </H1>
+            <Paragraph color="$text1" textAlign="right" fontSize="$3">
+              {texts.meetTheNewStandard}
+            </Paragraph>
+          </YStack>
+        </MotiView>
         <YStack flex={1} />
-        <YStack paddingHorizontal="$5" paddingVertical="$10" space="$4">
-          <Button
-            height="$6"
-            color="$text1"
-            size="$2"
-            borderColor="$royalBlue"
-            borderWidth="$1"
-            onPress={handleLoginPress}>
-            {texts.login}
-          </Button>
-          <Button
-            height="$6"
-            color="$text1"
-            size="$2"
-            backgroundColor="$royalBlue"
-            onPress={handleRegisterPress}>
-            {texts.register}
-          </Button>
-        </YStack>
+        <MotiView from={from} animate={animate} transition={transition}>
+          <YStack paddingHorizontal="$5" paddingVertical="$10" space="$4">
+            <Button
+              height="$6"
+              color="$text1"
+              size="$2"
+              borderColor="$royalBlue"
+              borderWidth="$1"
+              onPress={handleOpenBottomSheet}>
+              {texts.login}
+            </Button>
+            <Button
+              height="$6"
+              color="$text1"
+              size="$2"
+              backgroundColor="$royalBlue"
+              onPress={handleRegisterPress}>
+              {texts.register}
+            </Button>
+          </YStack>
+        </MotiView>
       </ScreenContainer>
       <BottomSheet
         ref={sheetRef}
