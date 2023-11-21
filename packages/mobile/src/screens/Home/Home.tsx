fc962e93eeded3434dc8c7f5d499bde72c61d0d8
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-20 22:39:37 -0300

    chore(improve-auth-flow): add uri error handle for home screen

--- packages/mobile/src/screens/Home/Home.tsx
+++ packages/mobile/src/screens/Home/Home.tsx
@@ -3,16 +3,36 @@ import { useMemo } from "react";
 import { RootStackReactNavigationProps } from "src/types/navigation";
 import { StyledWebView } from "./styles";
 import { getWebViewSource } from "./utils";
+import { Alert } from "react-native";
+import { texts } from "./texts";
+import { useNavigationForRootStack } from "@navigation/hooks";
 
 const Home = ({ route }: RootStackReactNavigationProps<"Home">) => {
+  const { navigate } = useNavigationForRootStack();
+
   const source = useMemo(
     () => getWebViewSource(route.params?.authToken),
-    [route.params.authToken],
+    [route.params?.authToken],
   );
+
+  if (!source.uri) {
+    Alert.alert(texts.oops, texts.weHaveAnErrorr, [
+      {
+        onPress: () => navigate("Landing"),
+      },
+    ]);
+
+    return null;
+  }
 
   return (
     <ScreenContainer background="$background2" statusBarStyle="dark">
-      <StyledWebView source={source} />
+      <StyledWebView
+        source={{
+          uri: source.uri,
+          headers: source.headers,
+        }}
+      />
     </ScreenContainer>
   );
 };
---
+++ packages/mobile/src/screens/Home/texts.ts
@@ -0,0 +1,7 @@
+const texts = {
+  oops: "🫠 Oops",
+  weHaveAnErrorr:
+    "We have an error loading your content, please, try again latter.",
+};
+
+export { texts };
