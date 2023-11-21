54657698c78452f8bccf1771af04493c30000ee2
ext.will.gomes <52263928+w1ll-dev@users.noreply.github.com>
2023-11-20 22:11:03 -0300

    chore(add-unit-test): improve jest setup with config file

--- packages/mobile/jest.config.ts
+++ packages/mobile/jest.config.ts
@@ -7,8 +7,9 @@ module.exports = {
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   testTimeout: 120000,
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|moti*)",
   ],
+  setupFiles: ["<rootDir>/jest/setup.ts"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   cacheDirectory: ".jest/cache",
   moduleDirectories: ["node_modules", "src"],
